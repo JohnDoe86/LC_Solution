0a8e7514545012fa477168033a818a10b6c2f9db
John <heguohao94@gmail.com>
2022-04-17 14:17:56 +0800

    Create LC8198.py

---
+++ LC8198.py
@@ -0,0 +1,34 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        tt = ""
+        for i in range(len(paragraph)):
+            if (paragraph[i]>= 'a' and paragraph[i] <='z') or (paragraph[i]>='A' and paragraph[i]<= 'Z'):
+                tt = tt + paragraph[i]
+            else:
+                tt += " "
+        print(tt)
+        word1 = tt.split(' ')
+        word = []
+        for w in word1:
+            temp = w.split()
+            for i in temp:
+                word.append(i.lower())
+        print(word)
+        b={}
+        for i in word:
+            if i not in b:
+                b[i] = 1
+            else:
+                b[i] += 1
+
+        for i in banned:
+            if i in b:
+                b.pop(i)
+        
+        cnt = 0
+        res = ""
+        for key in b:
+            if b[key] > cnt:
+                res = key
+                cnt = b[key]
+        return res
